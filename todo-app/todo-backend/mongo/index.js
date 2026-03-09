f03f6aced75138b41fa656312b47171a8c1d56dc
Pedro Araújo <pedroprog@gmail.com>
2026-03-09 12:07:42 +0000

    Exercise 9. Set up Redis for the project

--- todo-app/todo-backend/mongo/index.js
+++ todo-app/todo-backend/mongo/index.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose')
 const Todo = require('./models/Todo')
-const { MONGO_URL } = require('../util/config')
-
-//console.log('REDIS_URL: ', REDIS_URL)
+const { MONGO_URL,REDIS_URL } = require('../util/config')
+const redis = require('../redis')
 
 if (MONGO_URL && !mongoose.connection.readyState) {
   mongoose.connect(MONGO_URL)

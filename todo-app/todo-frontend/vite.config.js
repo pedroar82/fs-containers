8b73531121ee6001b95a9897b10fcb5e66edf519
Pedro Araújo <pedroprog@gmail.com>
2026-03-14 11:54:47 +0000

    Exercise 17. Set up an Nginx reverse proxy server in front of todo-frontend

--- todo-app/todo-frontend/vite.config.js
+++ todo-app/todo-frontend/vite.config.js
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 
+  server: {
+    allowedHosts: ['app', 'localhost'],
+  },
+
   test: {
     environment: 'jsdom',
     globals: true,
